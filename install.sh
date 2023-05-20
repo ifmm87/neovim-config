490d30c528758fd693b3ee048d29e7d2bd78a326
Ivan F. Mujica Mamani <ifmm87@gmail.com>
2023-05-20 18:54:14 -0400

    fix

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/sh
 
-NVIM_BEGINNER=~/.config/nvim-beginner
+NVIM_BEGINNER=~/.config/
 export NVIM_BEGINNER
 
-rm -rf $NVIM_BEGINNER
+rm -rf $NVIM_BEGINNER/nvim
 
-mkdir -p $NVIM_BEGINNER/share
 mkdir -p $NVIM_BEGINNER/nvim
 
 stow --restow --target=$NVIM_BEGINNER/nvim .
 
-alias nvb='XDG_DATA_HOME=$NVIM_BEGINNER/share XDG_CACHE_HOME=$NVIM_BEGINNER XDG_CONFIG_HOME=$NVIM_BEGINNER nvim'
